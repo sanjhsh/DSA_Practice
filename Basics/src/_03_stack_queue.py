962250ef4630774dbf3c3109c3e38bc52894eed1
Shakti Sharma <shaktisharma8318@gmail.com>
2026-06-24 23:27:29 +0530

    modified stack_prob_5

--- Basics/src/_03_stack_queue.py
+++ Basics/src/_03_stack_queue.py
@@ -266,7 +266,16 @@ def problem_5():
 
 def problem_6(temperatures):
     """Daily Temperatures - Find next warmer day for each day"""
-    pass
+    stack=[]
+    
+    for i in range(0,len(temperatures)):
+        for j in range(i+1,len(temperatures)):
+            if temperatures[j]>temperatures[i]:
+                stack.append(j-i)
+                break
+            else:
+                stack.append(0)
+    return stack
 
 
 def problem_7():
